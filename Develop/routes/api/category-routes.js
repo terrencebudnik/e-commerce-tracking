c8721396328d0ac25ee538cd498ea4eaee2fb10e
terrence <terrence.budnik@gmail.com>
2022-10-06 21:03:44 -0500

    finished routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -56,12 +56,44 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+    try {
+      const categoryData = await Category.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (!categoryData[0]) {
+        res.status(404).json({ message: 'No category with this id!' });
+        return;
+      }
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  router.delete('/:id', async (req, res) => {
+    try {
+      const categoryData = await Category.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (!categoryData) {
+        res.status(404).json({ message: 'No category found with that id!' });
+        return;
+      }
+  
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 });
 
 module.exports = router;
